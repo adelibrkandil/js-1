9fef3253c07847aec14bfd6bcc2f19f933126029
adelibrkandil <adelkandil120@gmail.com>
2016-07-14 23:10:02 +0200

    Add files via upload

---
+++ js 1.js
@@ -0,0 +1,10 @@
+﻿window.onload = function(){
+
+    var copy=document.getElementById("credit");
+    
+    if(copy==null){
+        window.location.href="http://itqanwebs.blogspot.com/";
+    };
+    copy.setAttribute("href","http://itqanwebs.blogspot.com/");
+    copy.innerHTML="إتقان ويب"
+};
